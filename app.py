5a93e51d092273d38e33999fa45ac2071f45cf12
Albatrous <42671837+Albatrous@users.noreply.github.com>
2019-02-27 19:59:48 +0700

    Testing bot

--- app.py
+++ app.py
@@ -113,5 +113,5 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT',33507))
     app.run(host='0.0.0.0', port=port)
