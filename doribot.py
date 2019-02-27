c4debd4afa1bcdd05d5bf3ece523d3dab6c14a69
Albatrous <42671837+Albatrous@users.noreply.github.com>
2019-02-27 19:00:44 +0700

    Testing bot

--- doribot.py
+++ doribot.py
@@ -113,4 +113,4 @@ def handle_message(event):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT',5000))
-    doribot.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
