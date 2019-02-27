b7a97c763d35bde1ea566cdd139a533bfcc3df7f
Albatrous <42671837+Albatrous@users.noreply.github.com>
2019-02-27 20:32:44 +0700

    Testing bot

--- app.py
+++ app.py
@@ -116,10 +116,5 @@ def handle_message(event):
 
 import os
 if __name__ == "__main__":
-    arg_parser = ArgumentParser(
-        usage='Usage: python '+__file__+' [--port <port>] [--help]'
-    )
-    arg_parser.add_argument('-p', '--port', type=int, default=5000, help='port')
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
-    options = arg_parser.parse_args()
-    app.run(debug=options.debug, port=options.port)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
