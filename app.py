c7307993c6dca90abe1726f6b454f29545f9e797
Albatrous <42671837+Albatrous@users.noreply.github.com>
2019-03-01 18:11:29 +0700

    Fix id checker

--- app.py
+++ app.py
@@ -240,7 +240,7 @@ def handle_message(event):
                 )
             else:
                 for user in db.child("users").get().val():
-                    if args == user["user_id"]:
+                    if args == db.child("users").child("user_id").get().val():
                         line_bot_api.reply_message(
                             event.reply_token,
                             TextSendMessage(text='Id is not available :(')
