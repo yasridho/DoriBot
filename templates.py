938eb5795f4dc17459b9e59d01420ac8fa908e5e
Albatrous <42671837+Albatrous@users.noreply.github.com>
2019-03-05 00:06:20 +0700

    Add google image search feature

--- templates.py
+++ templates.py
@@ -60,7 +60,10 @@ def gis(args,startIndex):
     udict = url.read().decode('utf-8')
     datagis = json.loads(udict)
     result = list()
-    nextPage = datagis["queries"]["nextPage"][0]["startIndex"]
+    try:
+        nextPage = datagis["queries"]["nextPage"][0]["startIndex"]
+    except:
+        nextPage = None
     for d in datagis["items"]:
         gambar = d["link"]
         if gambar[:7] == "http://":
@@ -221,7 +224,7 @@ def gis(args,startIndex):
                 )
             )
         )
-    if nextPage < 92:
+    if (nextPage < 92) or (nextPage != None):
         result.append(
             BubbleContainer(
                 direction='ltr',
